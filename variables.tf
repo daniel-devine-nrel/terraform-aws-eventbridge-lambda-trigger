11c9f412a36945ad66d5237ec1074ec535b6f233
Daniel Devine <daniel.devine@nrel.gov>
2023-02-22 16:32:34 -0700

    Added descriptions for variables

--- variables.tf
+++ variables.tf
@@ -5,14 +5,17 @@ variable "lambda_config" {
     lambda_role_arn     = string
     schedule_expression = string
   }))
+  description = "List of configurations for lambda functions to build triggers for."
 }
 
 variable "timezone" {
   type    = string
   default = "UTC"
+  description = "Timezone for AWS EventBrige cron expressions"
 }
 
 variable "aws_region" {
   type    = string
   default = "us-west-2"
+  description = "AWS region"
 }
